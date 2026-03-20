c0e55158f45a14f9f6c4c0aa35251bba7a4784e1
Daniel Sportelli <daniel.sportelli@gmail.com>
2026-03-20 23:29:46 +0100

    Update js.js

--- webapp/js/js.js
+++ webapp/js/js.js
@@ -2386,12 +2386,16 @@ document.getElementById("btn-favonly").addEventListener("click",function(){
     }
 
     // Reset a schermata cmds quando si riapre il drawer dall'esterno
-    var origOpen=window.openDrawer;
-    window.openDrawer=function(id){
-      if(id==='ris') showRisCmds();
-      if(id==='calc') showCalcCmds();
-      origOpen(id);
-    };
+    if(!window._openDrawerWrapped){
+      window._openDrawerWrapped = true;
+      var origOpen=window.openDrawer;
+      window.openDrawer=function(id){
+        if(id==='ris') showRisCmds();
+        if(id==='calc') showCalcCmds();
+        if(id==='acc') setTimeout(function(){ if(typeof renderAccountTab==='function') renderAccountTab(); }, 50);
+        origOpen(id);
+      };
+    }
 
     // Zone Temperature per drawer Risorse
     var ZONES=[
